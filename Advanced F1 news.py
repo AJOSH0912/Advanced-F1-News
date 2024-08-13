564f41e35978b64fc0487db2884683e656225688
AJOSH0912 <aryan.joshi@education.nsw.gov.au>
2024-08-13 11:49:20 +1000

    1st hour

--- Advanced F1 news.py
+++ Advanced F1 news.py
@@ -36,3 +36,26 @@ class F1NewsApp: #Creating a class for the F1 News App
         self.from_date_entry = ttk.Entry(filter_frame, width=10)
         self.from_date_entry.grid(row=0, column=5, padx=5)
         self.from_date_entry.insert(0, self.from_date)
+
+        ttk.Label(filter_frame, text="To date:").grid(row=0, column=6, sticky="w", padx=5)
+        self.to_date_entry = ttk.Entry(filter_frame, width=10)
+        self.to_date_entry.grid(row=0, column=7, padx=5)
+        self.to_date_entry.insert(0, self.to_date)
+        search_button = ttk.Button(filter_frame, text="Search", command=self.fetch_and_display_news) #Creates tge search button
+        search_button.grid(row=0, column=8, padx=10)
+        self.dark_mode = tk.BooleanVar()
+        theme_toggle = ttk.Checkbutton(filter_frame, text="Dark Mode", variable=self.dark_mode, command=self.toggle_theme)
+        theme_toggle.grid(row=0, column=9, padx=10)
+        self.news_frame = ttk.Frame(self.root)
+        self.news_frame.pack(fill="both", expand=True, padx=10, pady=10)
+        self.news_canvas = tk.Canvas(self.news_frame)
+        self.scroll_y = ttk.Scrollbar(self.news_frame, orient="vertical", command=self.news_canvas.yview)
+        self.scroll_frame = ttk.Frame(self.news_canvas)
+        self.scroll_frame.bind(
+            "<Configure>",
+            lambda e: self.news_canvas.configure(
+                scrollregion=self.news_canvas.bbox("all")
+            )
+        )
+
+#The following code above is for formating, it creates the gui and places the buttons and labels in the correct place. it also assigns the correct functions to the buttons.
